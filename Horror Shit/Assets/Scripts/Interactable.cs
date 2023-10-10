b34eea6e206ab79607dd1b35e43a655e078d6830
chithepepega <70763302+chithepepega@users.noreply.github.com>
2023-10-10 11:32:01 +0100

    interactable all

--- Horror Shit/Assets/Scripts/Interactable.cs
+++ Horror Shit/Assets/Scripts/Interactable.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public abstract class Interactable : MonoBehaviour
 {
     // message displayed to player when looking at an interactable.
+    public bool useEvents;
+    [SerializeField]
     public string promptMessage;
-
+    public virtual string OnLook()
+    {
+        return promptMessage;
+    }
     //this function will be called from our player
     public void BaseInteract()
     {
+        if (useEvents)
+            GetComponent<InteractionEvent>().onInteract.Invoke();
         Interact();
     }
     protected virtual void Interact()
